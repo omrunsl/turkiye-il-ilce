30b9bb5431ad87d857ff26e66b6df5f677f5a17a
Ömer Ünsal <omer.unsal@timurlar.local>
2018-10-20 13:38:44 +0300

    Update all files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 col-md-ofset-2 form-group">
+      <label class="my-1 mr-2" for="inlineFormCustomSelectPref">İl</label>
+      <select class="custom-select my-1 mr-sm-2 form-control" v-model="selected" @change="getIlce()" id="inlineFormCustomSelectPref">
+        <option v-if="selected == 'Seçiniz'">Seçiniz</option>
+        <option 
+          v-for="item in iller"  
+          v-bind:key="item.il"
+        >
+          {{item.il}}
+        </option>
+      </select>
+    </div>
+
+    <div class="col-md-4  form-group">
+      <label class="my-1 mr-2" for="inlineFormCustomSelectPref">İlçe</label>
+      <select class="custom-select my-1 mr-sm-2 form-control" id="inlineFormCustomSelectPref">
+        <option v-if="selectedIl == false">Seçiniz</option>
+        <option
+        v-for="ilce in ilceler"
+        v-bind:key="ilce"
+        >
+          {{ilce}}
+        </option>
+      </select>
+    </div>
+
+    <div class="col-md-4  form-group">
+      <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Bölge</label>
+      <input type="text" class="my-1 mr-sm-2 form-control" v-model="bolge['0']">
+    </div>
+    
   </div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      selected:'Seçiniz',
+      iller:{},
+      ilceler:[],
+      selectedIl:false,
+      bolge:[]
+    }
+  },
+  created() {
+    this.getIl()
+  },
+  methods:{
+    getIl() {
+      axios.get('json/il-bolge.json')
+      .then(res => this.iller = res.data)
+    },
+    getIlce() {
+      this.selectedIl = true
+      this.ilceler = []
+      this.bolge = []
+      axios.get('json/il-ilce.json')
+      .then(res => {
+        res.data.forEach(i => {
+          if(this.selected == i.il){
+            this.ilceler.push(i.ilce)
+            this.bolge.push(i.bolge)
+          }
+        })
+      })
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 Vue.config.productionTip = false
 
